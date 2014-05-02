04f5a2739b8001452fa6d577021039b5011b4444
Ryan Nash <nash@gust.com>
2014-05-02 16:40:52 -0400

    adding ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
+  def create
+    @post = current_user.posts.build(safe_params)
+    @post.save
+    redirect_to posts_path
+
+
+
+  end
+
+  private
+
+  def safe_params
+    params.require(:post).permit(:url)
+
+  end
 end
