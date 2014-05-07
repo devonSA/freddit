48404ac376d5a992fb6aabc865060d5e49f0ce16
Devon I <devon@gust.com>
2014-05-06 20:07:57 -0400

    adding downvote testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :votes
   def upvotes_count
-votes.where(polarity: true).count
+    votes.where(polarity: true).count
+  end
+  def downvotes_count
+    votes.where(polarity: false).count
+
   end
 end
